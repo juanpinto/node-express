7711220d27034943e453b03bf1784446b982b9a4
Juan Pinto <jpinto@grupov.com.ve>
2020-03-27 23:29:59 -0400

    Second Express Exercise using Morgan

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const http = require('http')
+const http = require('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(`${__dirname}/public`))
 
 app.use((request, response, next) => {
-    console.log(request.headers)
     response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html')
     response.end('<html><body><h1>This is an express server</h1></body></html>')
 })
 
